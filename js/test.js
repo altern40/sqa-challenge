ce28d8e9a94c1b2acf82e7b2680acedf521e6091
Ivan Morales <ivanesp47@gmail.com>
2023-09-13 23:49:09 -0500

    Commit 2 for the challenge

---
+++ js/test.js
@@ -0,0 +1,35 @@
+//Clock program from https://codepen.io/afarrar/pen/JRaEjP
+//Creator Aaron Farrar
+//Modified by Ivan Morales
+
+function clockDisplay(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    //var session = "AM";
+    
+    //Disabled 12 Hour Format
+    /*if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    */
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " ";
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(clockDisplay, 1000);
+    
+}
+
+clockDisplay();
